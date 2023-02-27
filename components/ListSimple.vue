44fb9b330d0368e1a3923d0f740ae5f020b7d52a
changjun <52chinaweb@gmail.com>
2023-02-27 09:48:39 +0800

    feat: text overflow

--- components/ListSimple.vue
+++ components/ListSimple.vue
@@ -12,7 +12,7 @@ const props = defineProps<{
 <template>
   <div class="w-auto m-auto text-left mb-3 flex flex-col border rounded-md p-3 mt-2">
     <NuxtLink v-for="(item, index) of props.data" :key="index" :to="item.link" :target="item.link?.includes('//') ? '_blank' : '_self'">
-      <p class="h-9 leading-9 hover:text-green-500 hover:font-bold">
+      <p class="truncate h-9 leading-9 hover:text-green-500 hover:font-bold">
         {{ index + 1 }}. {{ item.name }}
       </p>
     </NuxtLink>
